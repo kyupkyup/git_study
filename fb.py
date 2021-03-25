0693744d35e52712148822d9982ff7f6691b564d
kyupkyup <rudduqdl@naver.com>
2021-03-25 15:37:07 +0900

    init: Create fb.py to find fizz when input is multiple of 3 and to find buzz when input is multiple of 5

---
+++ fb.py
@@ -0,0 +1,21 @@
+
+
+class FizzBuzz:
+    def __init__(self, n):
+        self.number = n
+
+    def return_fizz():
+        pass
+
+    def return_buzz():
+        pass
+
+    def return_rest():
+        pass
+
+
+
+
+
+
+
